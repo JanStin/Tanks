9cd241cfe6f677d368432bf1388a6e0b35510193
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-03-28 17:57:12 +0300

    Created script on health player

---
+++ Assets/Scripts/PlayerCharater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharater : MonoBehaviour
+{
+    private int _health;
+    private readonly int _maxHealth = 100;
+
+    public void ChangeHealth(int value)
+    {
+        _health += value;
+
+        if (_health > _maxHealth)
+        {
+            _health = 100;
+        } 
+        else if (_health <= 0)
+        {
+            _health = 0;
+            Debug.Log("You lose...");
+        }
+
+    }
+}
