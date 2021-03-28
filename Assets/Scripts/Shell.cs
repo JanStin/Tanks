d10a10d1092847e81aad274be81213747c846909
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-03-28 11:06:22 +0300

    Created shell script and added on shell prefabs

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    private int _damage;
+    private float _lifeTime;
+
+    private void Start()
+    {
+        _damage = 20;
+        _lifeTime = 2f;
+
+        Destroy(gameObject, _lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy get damage " + _damage);
+            Destroy(gameObject);
+        }
+    }
+}
