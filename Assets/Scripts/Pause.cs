e4cd7a269139f339c029352d61889f15f107f733
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-03-31 23:30:04 +0300

    Created pause script. Changed GUI.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class Pause
+{
+    public static void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public static void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
