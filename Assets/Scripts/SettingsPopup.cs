c34e855c281394ed77670fba8dd84c27a302e6fe
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-04-06 22:36:44 +0300

    Created pre-menu

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopup : MonoBehaviour
+{
+    public void ChangeActive()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text healthText;
+    [SerializeField] private SettingsPopup settingsPopup;
+
+    private void Start()
+    {
+        settingsPopup.ChangeActive();
+    }
+
+    public void OnOpenSettings()
+    {
+        settingsPopup.ChangeActive();
+    }
+
+}
