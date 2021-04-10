29ba84a33218ab23eceb1bb9feebd802924e6e4d
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-04-10 12:08:04 +0300

    Created turrel trigger zone and rotation at player

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponentInParent<TurrelAI>().InTriggerZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponentInParent<TurrelAI>().InTriggerZone = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TurrelAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrelAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private int _health { get; set; }
+
+    public bool InTriggerZone;
+
+    private void Update()
+    {
+        if (InTriggerZone)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 3.0f);
+        }
+    }
+}
