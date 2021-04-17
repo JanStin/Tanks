6470f33a1f5e1926b938b83cd699d3a61003f39f
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-04-17 15:54:25 +0300

    Created base for spawn point

---
+++ Assets/Scripts/Managers/SpawnTurrels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTurrels : MonoBehaviour
+{
+    [SerializeField] private GameObject turrelPrefab;
+
+    public Transform Platform;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        Instantiate(turrelPrefab, Platform, true);
+    }
+}
